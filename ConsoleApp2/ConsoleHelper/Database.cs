70df83f1dde2878a1eced5f6383b4a95b5242bec
GustaPandini <gugui.pandini@gmail.com>
2026-01-14 22:24:57 -0300

    Adicionado a classe Database para conexão com o banco, baixado o Dapper

---
+++ ConsoleApp2/ConsoleHelper/Database.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ConsoleApp2.ConsoleHelper
+{
+    internal class Database
+    {
+        protected string conectionString = "Server=localhost;Database=concesionaria;Uid=root;Pwd=2017;";
+
+        protected MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(conectionString);
+        }
+
+        protected int Execute(string sql, object obj)
+        {
+            using (MySqlConnection conexao = GetConnection())
+            {
+                return conexao.Execute(sql, obj);
+            }
+        }
+    }
+}
