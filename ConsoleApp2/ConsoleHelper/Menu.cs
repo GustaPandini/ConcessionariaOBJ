e503cc354a4373ad409f50ce1fd85ad5b2785d2e
GustaPandini <gugui.pandini@gmail.com>
2026-02-18 22:05:08 -0300

    Aprimorada classe Menu

--- ConsoleApp2/ConsoleHelper/Menu.cs
+++ ConsoleApp2/ConsoleHelper/Menu.cs
@@ -3,49 +3,60 @@ using ConsoleApp2.Interfaces;
 
 namespace ConsoleApp2.ConsoleHelper
 {
-    public class Menu
+    public static class Menu
     {
         
         private static Icrud<Automovel> automovelConsole = new AutomovelConsole();
         public static void MenuPrincipal()
         {
-            Console.WriteLine("Bem vindo ao sistema de controle da concessionária Tainy");
-            Console.WriteLine("");
-            Console.WriteLine("-----------------------------------");
-            Console.WriteLine("1 = Adicionar Automóvel");
-            Console.WriteLine("2 = Ver lista de Automóveis");
-            Console.WriteLine("3 = Alterar Automóvel");
-            Console.WriteLine("4 = Excluir Automóvel");
-            Console.WriteLine("-----------------------------------");
-            Console.WriteLine();
+            while(true)
+            {
+                Console.WriteLine("Bem vindo ao sistema de controle da concessionária Tainy");
+                Console.WriteLine("");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("1 = Adicionar Automóvel");
+                Console.WriteLine("2 = Ver lista de Automóveis");
+                Console.WriteLine("3 = Alterar Automóvel");
+                Console.WriteLine("4 = Excluir Automóvel");
+                Console.WriteLine("0 = Encerrar programa");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine();
 
-            AcesosMenuPrincipal();
+                AcessosMenuPrincipal();
+            }
         }
 
-        public static void AcesosMenuPrincipal()
+        public static void AcessosMenuPrincipal()
         {
             
             Console.Write("Digite o numero da ação que deseja realizar = ");
-            int respostaMenu = Convert.ToInt32(Console.ReadLine());
+            if (!int.TryParse(Console.ReadLine(), out int respostaMenu))
+            {
+                Console.WriteLine("Opção inválida");
+                return;
+            }
 
-            switch (respostaMenu)
+                switch (respostaMenu)
             {
                 case 1:
                     automovelConsole.Inserir();
-                    MenuPrincipal();
                     break;
                 case 2:
                     automovelConsole.Listar();
-                    MenuPrincipal();
                     break;
                 case 3:
                     automovelConsole.Alterar();
-                    MenuPrincipal();
                     break;
                 case 4:
                     automovelConsole.Deletar();
-                    MenuPrincipal();
                     break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+
             }
         }
     }
